cc3a9268e1b2fa7208a4c34471bf05c340aa218c
270390 <>
2019-07-29 03:12:19 +0100

    Added comments for Game View Model interface

--- TestXO/View Models/GameViewModel/GameViewModel.swift
+++ TestXO/View Models/GameViewModel/GameViewModel.swift
@@ -12,8 +12,28 @@ protocol GameViewModel {
     
     typealias feedbackClosure = (_ gameCompleted: Bool, _ winningMoves: [Position]?, _ winningPlayer: Player?) -> ()
     
+    /// Returns: total number of grid items
+    ///
+    /// - Returns: total number of grid items
     func getNumberOfGridItems () -> Int
+    
+    /// Returns: number of grid items per row
+    ///
+    /// - Returns: number of grid items per row
     func getNumberOfItemsPerRow () -> Int
+    
+    /// ends player turn after action
+    ///
+    /// - Parameters:
+    ///   - position: the position in the grid for the new action
+    ///   - player: the player doing the action
+    ///   - completion: callback after the action has been evaluated
+    /// - Returns: void
     func endTurnWithMove (position: Position, player: Player, completion: feedbackClosure?)
+    
+    /// gets player data
+    ///
+    /// - Parameter order: can be previous player, current player or next player
+    /// - Returns: the player
     func getPlayer(order: PlayerOrder) -> Player
 }
